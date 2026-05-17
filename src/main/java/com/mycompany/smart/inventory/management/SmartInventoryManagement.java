bc454a13560f41f7caa57f938cc2c2597d085a8b
DanarGdg <danargading@gmail.com>
2026-05-17 16:57:50 +0700

    initial commit

---
+++ src/main/java/com/mycompany/smart/inventory/management/SmartInventoryManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.smart.inventory.management;
+
+/**
+ *
+ * @author danar
+ */
+public class SmartInventoryManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
